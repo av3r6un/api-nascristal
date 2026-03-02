f72ae711199c4f1be79ffd646ff162e1456029e5
av3r6un <av3rgun@yandex.com>
2026-03-02 16:26:49 +0300

    Added new file

---
+++ src/api/__init__.py
@@ -0,0 +1,15 @@
+# from .auth import router as auth_router
+# from .feedback import router as feedback_router
+# from .i18n import router as i18n_router
+# from .catalog import router as catalog_router
+# from .static import router as static_router
+from .onec import router as onec_router
+
+# routers = (
+#   auth_router,
+#   feedback_router,
+#   i18n_router,
+#   catalog_router,
+#   static_router,
+#   onec_router,
+# )
