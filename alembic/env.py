8be52a6157e9dd5ead3835a4fb154923b41fd087
av3r6un <av3rgun@yandex.com>
2026-03-03 22:42:00 +0300

    Fixing alembic

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,7 @@ from src.models.base import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.db_url.replace('+aiomysql', '+pymysql'))
+config.set_main_option('sqlalchemy.url', settings.db_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
